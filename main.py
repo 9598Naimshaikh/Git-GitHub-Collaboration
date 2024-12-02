2725a85c52219cefd09003fb2f5b1facc7d77c65
9598Naimshaikh <nn9412466@gmail.com>
2024-12-02 11:33:32 +0530

    Code of Factorial Calculation is done

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # write a function to calculate factorial of the given number.
 def factorial_calc(num):
     # write down your logic here..
-    pass
+    fact = 1
+    if num < 0:
+        print("Please enter positive number.")
+    else:
+        for i in range(num):
+            fact = num * fact
+            num = num-1
+    print(fact)
+
 
 
 # write a function to calculate n number of times to calculate fibonacchi number.
@@ -14,3 +22,7 @@ def fibonacchi_calc(num_of_term):
 def palindrome_checker(num):
     # write down your logic here..
     pass
+
+
+if __name__ == "__main__":
+    factorial_calc(5)
