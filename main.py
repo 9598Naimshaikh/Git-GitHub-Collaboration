86a5e1bfe3a3e872e091fdc969d6fb22eaab8e78
9598Naimshaikh <nn9412466@gmail.com>
2024-12-02 11:13:13 +0530

    Initial Code 0

---
+++ main.py
@@ -0,0 +1,16 @@
+# write a function to calculate factorial of the given number.
+def factorial_calc(num):
+    # write down your logic here..
+    pass
+
+
+# write a function to calculate n number of times to calculate fibonacchi number.
+def fibonacchi_calc(num_of_term):
+    # write down your logic here..
+    pass
+
+
+# write a function to check given number is palindrome or not.
+def palindrome_checker(num):
+    # write down your logic here..
+    pass
